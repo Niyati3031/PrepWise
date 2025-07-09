f5932121a84ab5064f8eef5a63d4a7424adf659c
Niyati Soni <niya.legend31@gmail.com>
2025-07-10 00:57:30 +0530

    Changed card feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -7,6 +7,9 @@ import DisplayTechIcons from "./DisplayTechIcons";
 
 import { cn, getRandomInterviewCover } from "@/lib/utils";
 import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const currentUser = await getCurrentUser();
 
 const InterviewCard = async ({
   id,
@@ -17,7 +20,7 @@ const InterviewCard = async ({
   createdAt,
 }: InterviewCardProps) => {
   const feedback =
-    userId && id
+    userId && id && (userId == currentUser)
       ? await getFeedbackByInterviewId({
           interviewId: id,
           userId,
